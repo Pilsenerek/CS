8a881bed80cae426d093ea8790d09197a2562d6a
Michał Zbieranek <michalzbieranek@wp.pl>
2024-09-06 23:58:27 +0200

    Security

--- src/Controller/LoanController.php
+++ src/Controller/LoanController.php
@@ -6,11 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class LoanController extends AbstractController
 {
 
     #[Route('/loan/calculate', name: 'loan_calculate_list', methods: ['GET'])]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
     public function list(): JsonResponse
     {
         return $this->json([__METHOD__]);
@@ -23,6 +25,7 @@ class LoanController extends AbstractController
     }
 
     #[Route('/loan/calculate/{loanId<\d+>}/status', name: 'loan_calculate_status', methods: ['PATCH'])]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
     public function status(Request $request): JsonResponse
     {
         return $this->json([__METHOD__ => $request->get('loanId')]);
---
+++ src/Security/AccessTokenHandler.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Security;
+
+use App\Repository\AccessTokenRepository;
+use Symfony\Component\Security\Core\Exception\BadCredentialsException;
+use Symfony\Component\Security\Http\AccessToken\AccessTokenHandlerInterface;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+
+class AccessTokenHandler implements AccessTokenHandlerInterface
+{
+    public function getUserBadgeFrom(string $accessToken): UserBadge
+    {
+        //@todo move token to .env
+        if ($accessToken === 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.e30.Et9HFtf9R3GEMA0IICOfFMVXY7kkTX1wr4qCyhIf58U') {
+
+            return new UserBadge('apiKey');
+        }
+
+        throw new BadCredentialsException('Invalid credentials.');
+    }
+}
